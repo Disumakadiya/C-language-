12b45cc92dc83b992623fd3e2ecaf701d3992f5b
Disumakadiya <disumakadiya@gmail.com>
2024-07-29 17:44:38 +0530

    Add files via upload

---
+++ practical.c
@@ -0,0 +1,19 @@
+/*Name: Disu Makadiya
+Date of program:16/7/2024
+Aim:calculate using addition operator
+*/
+
+#include<stdio.h>
+
+void main()
+{
+  int x,y,z;
+  //additiona operater used 
+  scanf("%d\n",&x);
+  scanf("%d\n",&y);
+  //formula to calculate x and y
+  z=x+y;
+  //display the value of z
+  printf("%d",z);
+}
+    
