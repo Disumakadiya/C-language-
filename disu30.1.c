ff078194f7b66eeb9ab971e1fbbd27a88423e4d7
Disumakadiya <disumakadiya@gmail.com>
2024-11-26 09:14:12 +0530

    Update disu30.1.c

--- disu30.1.c
+++ disu30.1.c
@@ -1,7 +1,7 @@
 /*
 name:DISU MAKADIYA
 date:25-NOV'24
-aim:a C program to read a text file ‘Demo.txt’ and print each word of that file
+aim:a C program to read a text file â€˜Demo.txtâ€™ and print each word of that file
 in reverse order.
 */
 
@@ -11,32 +11,36 @@ void main()
     FILE *file;
     char name[50],rev_note[50];
     int i,j,l=0;
+    //open the file jn read mode
     file=fopen("Demo.txt","r");
-    printf("Enter the word:");
+    printf("Enter the word:");//enter the word 
     gets(name);
     fclose(file);
     file=fopen("Demo.txt","w");
 
   if(file==NULL)
      {
-         printf("error!!file not found\n");
+         printf("error!!file not found\n");//exist if file not found
      }
      else
      {
-         while(!feof(file))
+         //read the words from the file
+         while(!feof(file))//stops when end of file 
+             
     for(i=0; name[i]!='\0'; i++)
     {
-        l++;
+        l++;//length of word
     }
     for(i=(l-1),j=0; i>=0; i--,j++)
     {
+        //reverse the letter
         rev_note[j]=name[i];
     }
     rev_note[j]='\0';
-    printf("Reverse note:");
+    printf("Reverse note:");//print the letter in reverse form
     puts(rev_note);
      }
-    fclose(file);
+    fclose(file);//close file 
     printf("*********\n");
     printf("DISU MAKADIYA \n 24CE062");
 
